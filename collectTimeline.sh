9988420ec8ec2ecb7d9c6f60a454cd3899c9470b
Ming Lyu (CareF) <minglyu@google.com>
2020-06-29 11:41:31 -0400

    Update timelne

--- collectTimeline.sh
+++ collectTimeline.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # require branch https://github.com/CareF/flutter/tree/widgetTester_with_time
+echo E2E perf test
 FLUTTER_TEST_OUTPUTS_DIR=$(pwd) flutter drive -t test_driver/e2e_perf.dart -d pixel --profile  --trace-startup
+echo flutter_driver perf test
 FLUTTER_TEST_OUTPUTS_DIR=$(pwd) flutter drive -t test_driver/driver_perf.dart -d pixel --profile  --trace-startup
