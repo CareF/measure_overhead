9edda37ce728ae6c9e11b361456ddf2b3084fb0e
Ming Lyu (CareF) <minglyu@google.com>
2020-06-28 23:09:51 -0400

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'src/multi_widget_construction.dart';
 
 void main() {
-  runApp(const MultiWidgetConstructTable(10, 10));
+  runApp(const MaterialApp(home: MultiWidgetConstructTable(10, 10)));
 }
