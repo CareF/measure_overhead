e8f29d09b5caa631e642d5cc6475372d7d0e3fa7
Ming Lyu (CareF) <minglyu@google.com>
2020-06-30 13:44:36 -0400

    Use fullylive and remove pump

--- test_driver/e2e_perf.dart
+++ test_driver/e2e_perf.dart
@@ -2,13 +2,15 @@ import 'package:e2e/e2e.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:measure_overhead/main.dart' as app;
 
-void main() {
+Future<void> main() async {
   final E2EWidgetsFlutterBinding binding =
-      E2EWidgetsFlutterBinding.ensureInitialized() as E2EWidgetsFlutterBinding;
+  E2EWidgetsFlutterBinding.ensureInitialized() as E2EWidgetsFlutterBinding;
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
-  testWidgets('test e2e overhead', (WidgetTester tester) async {
+  testWidgets('E2E perf test', (WidgetTester tester) async {
+    //await benchmarkWidgets((WidgetTester tester) async {
     app.main();
-    await tester.pump();  // Wait for the first frame.
-    await tester.pumpContinuous(const Duration(seconds: 10), frequency: 90.0);
+    await Future<void>.delayed(const Duration(seconds: 10));
+    // await tester.pump();  // Wait for the first frame.
+    // await tester.pumpContinuous(const Duration(seconds: 10), frequency: 90.0);
   }, semanticsEnabled: false);
 }
