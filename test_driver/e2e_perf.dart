89538648bbc342247436e87320b9881de2bf8555
Ming Lyu (CareF) <minglyu@google.com>
2020-06-30 13:39:22 -0400

    update e2e

--- test_driver/e2e_perf.dart
+++ test_driver/e2e_perf.dart
@@ -4,10 +4,10 @@ import 'package:measure_overhead/main.dart' as app;
 
 Future<void> main() async {
   final E2EWidgetsFlutterBinding binding =
-  E2EWidgetsFlutterBinding.ensureInitialized() as E2EWidgetsFlutterBinding;
+      E2EWidgetsFlutterBinding.ensureInitialized() as E2EWidgetsFlutterBinding;
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
   testWidgets('E2E perf test', (WidgetTester tester) async {
-    //await benchmarkWidgets((WidgetTester tester) async {
+  //await benchmarkWidgets((WidgetTester tester) async {
     app.main();
     await Future<void>.delayed(const Duration(seconds: 10));
     // await tester.pump();  // Wait for the first frame.
